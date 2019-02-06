ccbf5872b6ac2f1fcce7e91cc7bf8a215cde17a1
Rhys <rhysrichmond1@gmail.com>
2019-02-06 16:13:24 +0000

    I have added the current folder structure for the unity game, I have included the assests folder and the project settings, I currently have the slingshot script which I am working on currently yyou can drag the player and then pull them back and then when you let them go it will fire them however currently I am ttesting and launching the player isn't working at the moment

---
+++ Programming/Rhys Richmond/Assets/Scripts/Character/Slingshot.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Slingshot : MonoBehaviour {
+
+    private SpringJoint2D sj;
+    private Rigidbody2D rb;
+    public GameObject ResetPoint;
+    private bool IsPressed;
+    public float force;
+    public bool CanBeLanched = true;
+
+    void Start () {
+        rb = this.gameObject.GetComponent<Rigidbody2D>();
+        sj = this.gameObject.GetComponent<SpringJoint2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        CharacterDrag();
+    }
+
+    private void OnMouseDown()
+    {
+        IsPressed = true;
+        rb.isKinematic = true;
+    }
+
+    private void OnMouseUp()
+    {
+        IsPressed = false;
+        rb.isKinematic = false;
+        Debug.Log("This is letting go");
+        sj.attachedRigidbody.AddForce(Vector2.right * force);
+        sj.enabled = false;
+        CanBeLanched = false;
+    }
+
+    void CharacterDrag() {
+        if (Input.GetMouseButton(0)){
+            if (CanBeLanched == true) {
+                Vector2 MousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                rb.transform.position = MousePos;
+            }
+        }
+    }
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Platform") {
+            this.gameObject.transform.position = ResetPoint.transform.position;
+            sj.enabled = true;
+            CanBeLanched = true;
+            }
+    }
+}
