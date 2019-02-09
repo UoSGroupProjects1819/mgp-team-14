ab3623711e9ebd14b88adb25266c4b1818f6aff6
Rhys <rhysrichmond1@gmail.com>
2019-02-09 17:04:21 +0000

    I have now fixed the launching of the player using the rigid body and the spring joint as the way of giving the player momentum. I have also adding some smaller things which will let it move a lot smoother such as adding a delay so that it releases to the middle and then fires. This was done through courentines and calculating the delay using the spring joint. I have also added the projectile line which uses the line renderer component and then uses the two positions of the spring joint and player to draw a line.

--- Programming/Rhys Richmond/Assets/Scripts/Character/Slingshot.cs
+++ Programming/Rhys Richmond/Assets/Scripts/Character/Slingshot.cs
@@ -6,14 +6,25 @@ public class Slingshot : MonoBehaviour {
 
     private SpringJoint2D sj;
     private Rigidbody2D rb;
+    private Rigidbody2D SlingRb;
+    private LineRenderer LRender;
     public GameObject ResetPoint;
+
     private bool IsPressed;
-    public float force;
     public bool CanBeLanched = true;
 
-    void Start () {
+    private float MaxDist = 2f;
+    private float delay;
+
+    void Awake () {
         rb = this.gameObject.GetComponent<Rigidbody2D>();
         sj = this.gameObject.GetComponent<SpringJoint2D>();
+        LRender = this.gameObject.GetComponent<LineRenderer>();
+
+        SlingRb = sj.connectedBody;
+        LRender.enabled = false;
+
+        delay = 1 / (sj.frequency * 4);
     }
 
     private void FixedUpdate()
@@ -25,25 +36,46 @@ public class Slingshot : MonoBehaviour {
     {
         IsPressed = true;
         rb.isKinematic = true;
+        LRender.enabled = true;
     }
 
     private void OnMouseUp()
     {
         IsPressed = false;
         rb.isKinematic = false;
-        Debug.Log("This is letting go");
-        sj.attachedRigidbody.AddForce(Vector2.right * force);
-        sj.enabled = false;
         CanBeLanched = false;
+        LRender.enabled = false;
+        StartCoroutine(Release());
     }
 
     void CharacterDrag() {
-        if (Input.GetMouseButton(0)){
+        if (IsPressed){
             if (CanBeLanched == true) {
+                SetLRenderPos();
                 Vector2 MousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-                rb.transform.position = MousePos;
+                float CurrentDist = Vector2.Distance(MousePos, SlingRb.position);
+                if (CurrentDist > MaxDist)
+                {
+                    Vector2 direction = (MousePos - SlingRb.position).normalized;
+                    rb.position = SlingRb.position + direction * MaxDist;
+                }
+                else {
+                    rb.position = MousePos;
+                }
             }
         }
+    }
+    private IEnumerator Release() {
+        yield return new WaitForSeconds(delay);
+        sj.enabled = false;
+
+    }
+
+    private void SetLRenderPos() {
+        Vector3[] Positions = new Vector3[2];
+        Positions[0] = rb.position;
+        Positions[1] = SlingRb.position;
+        LRender.SetPositions(Positions);
     }
 
     public void OnTriggerEnter2D(Collider2D collision)
