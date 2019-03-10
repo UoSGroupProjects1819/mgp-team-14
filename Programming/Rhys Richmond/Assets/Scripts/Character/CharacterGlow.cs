49ce632b16dfc767efe80364f5bd8bdebce9ba6b
Rhys <rhysrichmond1@gmail.com>
2019-03-10 13:40:03 +0000

    I have added a number of things, first of all being trying to fix the glow however I still don't really understand how it is going to work with the light not showing if at anypoint I figure it out then I will change it, but I have a bunch of shaders which I was testing to find an alternative option. In have also made a new platform which resets the player it works a lot like the kill box and is made the same way, this is a new prefab. The last thing which I have done is to make the facial expessions change with collectibles picked up and this works the same way as the glow did.

--- Programming/Rhys Richmond/Assets/Scripts/Character/CharacterGlow.cs
+++ Programming/Rhys Richmond/Assets/Scripts/Character/CharacterGlow.cs
@@ -8,17 +8,29 @@ public class CharacterGlow : MonoBehaviour {
     private Color GlowColour;
     //private Color StartingGlow = new Color32(252, 91, 83, 255);
     private GameObject Character;
-	
-	void Start () {
+    private GameObject Face;
+    private SpriteRenderer SR;
+    public Sprite[] Sprites;
+
+    void Start () {
         GlowCharacter = GetComponent<Light>();
         Character = this.gameObject;
+        Face = GameObject.Find("Face");
+        SR = Face.GetComponent<SpriteRenderer>();
+        
         GlowColour = Color.red;
 	}
 
     void Update() {
         GlowCharacter.color = GlowColour;
-        if (Character.GetComponent<Slingshot>().CollectibleCounter == 1) {
+        if (Character.GetComponent<Slingshot>().CollectibleCounter == 1)
+        {
             GlowColour = Color.magenta;
+            SR.sprite = Sprites[1];
+        }
+        else if (Character.GetComponent<Slingshot>().CollectibleCounter == 2) {
+            SR.sprite = Sprites[2];
+
         }
     }
 	
